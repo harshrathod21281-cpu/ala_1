4cdfa943b219309c16e763c8ce8f499a1d12075e
harshrathod21281-cpu <harshrathod21281@gmail.com>
2026-02-21 20:05:23 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,27 @@
+print("Digit Frequency Counter")
+
+num = int(input("Enter number: "))
+
+zero = 0
+other = 0
+
+while num > 0:
+    digit = num % 10
+    
+    if digit == 0:
+        zero = zero + 1
+    else:
+        other = other + 1
+    
+    num = num // 10
+
+print("Zero digits:", zero)
+print("Other digits:", other)
+
+if zero > other:
+    print("More zeros")
+else:
+    print("More non-zero digits")
+
+total = zero + other
+print("Total digits:", total)
